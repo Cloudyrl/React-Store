1cf9a75c9f135ee162025ef4a220d4f918b3a908
Rommel Liendo <cloudyrl1@gmail.com>
2020-03-19 13:12:47 -0400

    basic routing logic using react-router-dom added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import {Switch,Route} from 'react-router-dom'
+
+import Home from './pages/home/home.component'
+import About from './pages/about/about.component'
+import Cart from './pages/cart/cart.component'
+import Checkout from './pages/checkout/checkout.component'
+import Error from './pages/error/error.component'
+import Login from './pages/login/login.component'
+import ProductDetail from './pages/product-detail/product-detail.component'
+import Products from './pages/products/products.component'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/about">
+            <About/>
+          </Route>
+          <Route path="/login">
+            <Login/>
+          </Route>
+          <Route 
+          path="/products/:id"
+          children={<ProductDetail/>}
+          >  
+          </Route>
+          <Route path="/products">
+            <Products/>
+          </Route>
+          <Route path="/cart">
+            <Cart/>
+          </Route>
+          <Route path="/checkout">
+            <Checkout/>
+          </Route>
+          <Route path="*">
+            <Error/>
+          </Route>
+        </Switch>
     </div>
   );
 }
--- src/pages/product-detail/product-detail.component.jsx
+++ src/pages/product-detail/product-detail.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import {useParams} from 'react-router-dom'
 
 const ProductDetail = () => {
+    const { id } = useParams();
+
     return(
-        <h1>hello from product detail</h1>
+    <h1>hello from product detail id = {id}</h1>
     )
 }
 
