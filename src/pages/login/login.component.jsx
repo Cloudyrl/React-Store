79134631fa9909788802a2e3c00418b34f64bec9
Rommel Liendo <cloudyrl1@gmail.com>
2020-05-06 23:23:23 -0400

    login page structure an styles created

--- src/pages/login/login.component.jsx
+++ src/pages/login/login.component.jsx
@@ -1,9 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import "./login.styles.scss";
 
 const Login = () => {
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("default");
+  const [isMember, setIsMember] = useState(true);
+
+  let isEmpty = !email || !password || !username;
+
+  const toggleMember = () => {
+    setIsMember(prevMember =>{
+      let isMember = !prevMember
+      isMember?setUsername('default'):setUsername('')
+      return isMember;
+    })
+  };
+
+  const handleSubmit = async e => {
+    // alert
+    e.preventDefault();
+    let response;
+
+    if(isMember){
+      // response = await loginUser
+    }else{
+      // response = await registerUser
+    }
+
+    if(response){
+      //
+    }else{
+      // show alert
+    }
+  };
+
   return (
-    <div>
-      <h1>hello from login page</h1>
+    <div className="login-container">
+      <h1>{isMember ? "Sign in" : "Register"}</h1>
+      <Form>
+        {!isMember && (
+          <Form.Group controlId="Username">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" placeholder="Username" onChange={e=> setUsername(e.target.value)}/>
+          </Form.Group>
+        )}
+
+        <Form.Group controlId="Email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" onChange={e=> setEmail(e.target.value)}/>
+        </Form.Group>
+
+        <Form.Group controlId="Password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" onChange={e=> setPassword(e.target.value)}/>
+        </Form.Group>
+
+        {isEmpty && <p className="empty">Please fill out all fields</p>}
+
+        {!isEmpty && (
+          <Button variant="primary" type="submit" onClick={handleSubmit} block>
+            Submit
+          </Button>
+        )}
+        <p className="toggle">
+        {isMember ? "Need to register?": "Already a member?"}
+        <button type="button" onClick={toggleMember}>click here</button>
+        </p>
+      </Form>
     </div>
   );
 };
