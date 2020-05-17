012b4f0105cadb1017c34b7c20cbd597886bfa1e
Rommel Liendo <cloudyrl1@gmail.com>
2020-05-17 14:41:46 -0400

    home-image improvements

--- src/components/home-image/home-image.component.jsx
+++ src/components/home-image/home-image.component.jsx
@@ -6,7 +6,8 @@ import "./home-image.styles.scss";
 const HomeImage = () => {
   return (
     <div className="image-container">
-      <div className="banner">
+      <div className="banner"></div>
+      <div className="content">
         <h6>Shop with us!</h6>
         <Link to="/shop" className="products-btn">
         Products
